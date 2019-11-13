803dbf12faf0975c51535cf3a53519fdf6ba2f8b
Svetha <spulav>
2019-11-13 13:46:00 -0500

    added starter for lookup.py

---
+++ draft/lookup.py
@@ -0,0 +1,12 @@
+import dbi
+
+DSN = None
+
+def getConn(db):
+    '''returns a database connection to the given database'''
+    global DSN
+    if DSN is None:
+        DSN = dbi.read_cnf()
+    conn = dbi.connect(DSN)
+    conn.select_db(db)
+    return conn
