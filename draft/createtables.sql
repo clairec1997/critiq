f0d5bccefdcde1d34463f0e4f050bdb5e7b21e36
Sophia <sophbussey>
2019-11-13 14:47:07 -0500

    added stuff

--- draft/createtables.sql
+++ draft/createtables.sql
@@ -1,10 +1,11 @@
 drop table if exists works;
 drop table if exists users;
+drop table if exists chapters;
 
 
 create table users {
     int uid not null auto_increment primary key,
-    varchar username, 
+    varchar(30) username, 
     BINARY(64) passhash,
     float commentscore
 }
@@ -21,6 +22,10 @@ create table works {
             'Suicidal Content','Abuse', 'Animal Cruelty', 'Self-Harm', 'Eating Disorder',
             'Incest','Child Abuse/Pedophilia', 'Death/Dying','Pregnancy/Childbirth',
             'Miscarriages/Abortion','Mental Illness'),
+    enum isFin('Finished','Work in Progress'),
+    date updated,
+    varchar(2000) summary,
+    float stars,
 
     index(uid)
     foreign key (uid) references users(uid)
@@ -31,14 +36,28 @@ create table works {
 ENGINE = InnoDB;
 
 create table chapters {
-    int cnum
-    int sid not NULL    
+    int cid not null auto_increment,
+    int cnum,
+    int sid not NULL,    
 
     index(sid)
     foreign key (sid) references works(sid)
         on update cascade
         on delete cascade
 
-    Primary key (cnum, sid)
+    Primary key (cid)
 }
 
+Engine = InnoDB;
+
+create table comments {
+    int cid not null,
+    int commenter not null,
+    int ishelpful,
+    varchar(2000),
+}
+
+create table reviews {
+    int sid not null,
+
+}
