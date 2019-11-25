6c2528dce49ecf3a117582c4f16636ec49dbb63a
Svetha <spulav>
2019-11-24 19:35:07 -0500

    remove notification from tinyeditor

--- draft/static/tinyeditor.js
+++ draft/static/tinyeditor.js
@@ -1,11 +1,13 @@
 tinymce.init({
     selector:'#tinytext',
-    plugins: ['wordcount hr spellchecker'],
-    toolbar: ['undo redo | bold italic underline strikethrough |', 
-            'alignleft aligncenter alignright |',
-            'bullist numlist | blockquote hr | removeformat'],
+    plugins: ['wordcount hr spellchecker lists'],
+    toolbar: ['undo redo | bold italic underline strikethrough | alignleft aligncenter alignright | bullist numlist | blockquote hr indent outdent | removeformat'],
     menubar: false,
-    width: 600, //px
-    height: 300,
+    width: 900, //px
+    height: 600,
     element_format: 'html',
+    init_instance_callback : function(editor) {
+        var freeTiny = document.querySelector('.tox .tox-notification--in');
+       freeTiny.style.display = 'none';
+      }
 });
