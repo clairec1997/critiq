f2541c613d3a604345d879e54d219fe53da75d81
Svetha <spulav>
2019-11-26 18:10:41 -0500

    trying to figure out how to preset content

--- draft/static/tinyeditor.js
+++ draft/static/tinyeditor.js
@@ -2,14 +2,14 @@ tinymce.init({
     selector:'#tinytext',
     plugins: ['wordcount hr spellchecker lists'],
     toolbar: ['undo redo | bold italic underline strikethrough | alignleft aligncenter alignright | bullist numlist | blockquote hr indent outdent | removeformat'],
-    menubar: false,
+    menubar: false, //sorry scott that line above needs to be long
     width: 700, //px
     height: 600,
     element_format: 'html',
-    init_instance_callback : function(editor) {
+    //init_instance_callback : function(editor) {
         //var freeTiny = document.querySelector('.tox .tox-notification--in');
        //freeTiny.style.display = 'none';
-        editor.setContent(story+" html");
-        console.log(story);
-      };
+      //  editor.setContent(story+" html");
+        //console.log(story);
+      //};
 });
